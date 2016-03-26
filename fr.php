d0a9563599a92010c8496d955bd4d6fc9915c69d
mick59495 <mick59495@hotmail.fr>
2016-03-26 15:03:20 +0100

    Update fr.php

--- fr.php
+++ fr.php
@@ -9,7 +9,6 @@
 //                                Adding tasks, constructions and artefact  by: Armando             //
 //                                Modified , added , fixed , implementd  by: Shadow and ronix       //
 //////////////////////////////////////////////////////////////////////////////////////////////////////
-									//         -Test-          //
 									//         French          //
 									// Author: TiSmA - Mickou  //
 									/////////////////////////////
